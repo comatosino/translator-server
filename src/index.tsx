3f38eebe3e2216fa168f80fde38d28661fd84bb6
Robert Adams <adamsiii.robert@gmail.com>
2022-02-19 11:36:43 -0800

    renamed component

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import Translator from "./Translator";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Translator />
   </React.StrictMode>,
   document.getElementById("root")
 );
