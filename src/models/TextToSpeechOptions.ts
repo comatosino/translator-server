cc9c069460207f00db9bba1107c4397e28a077e2
Robert Adams <adamsiii.robert@gmail.com>
2022-02-19 11:36:12 -0800

    added hook state interface

---
+++ src/models/TextToSpeechOptions.ts
@@ -0,0 +1,7 @@
+export default interface TextToSpeechOptions {
+  lang: string;
+  voice: SpeechSynthesisVoice | null;
+  pitch: number; // range between 0 (lowest) and 2 (highest)
+  rate: number; // between 0.1 (lowest) and 10 (highest)
+  volume: number; // between 0 (lowest) and 1 (highest)
+}
