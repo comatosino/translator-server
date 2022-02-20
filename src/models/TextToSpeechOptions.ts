fb296aed313f300e335ce40549a5d86f7f543dc9
Robert Adams <adamsiii.robert@gmail.com>
2022-02-19 18:52:31 -0800

    added union type to match event handler type ecpectations

--- src/models/TextToSpeechOptions.ts
+++ src/models/TextToSpeechOptions.ts
@@ -1,7 +1,6 @@
 export default interface TextToSpeechOptions {
-  lang: string;
-  voice: SpeechSynthesisVoice | null;
-  pitch: number; // range between 0 (lowest) and 2 (highest)
-  rate: number; // between 0.1 (lowest) and 10 (highest)
-  volume: number; // between 0 (lowest) and 1 (highest)
+  voice: string;
+  pitch: number | number[]; // range between 0 (lowest) and 2 (highest)
+  rate: number | number[]; // between 0.1 (lowest) and 10 (highest)
+  volume: number | number[]; // between 0 (lowest) and 1 (highest)
 }
