1864e0636cb9cd67b22e1f2acb178a240ceae888
Robert Adams <adamsiii.robert@gmail.com>
2022-02-20 17:19:40 -0800

    renamed field

--- src/classes/SpeechToText/index.ts
+++ src/classes/SpeechToText/index.ts
@@ -1,40 +1,40 @@
 export default class SpeechToText {
   private static _instance: SpeechToText;
-  public manager: SpeechRecognition;
+  public interface: SpeechRecognition;
 
   private constructor() {
-    this.manager = new webkitSpeechRecognition();
-    this.manager.onstart = (e: Event) => {
+    this.interface = new webkitSpeechRecognition();
+    this.interface.onstart = (e: Event) => {
       console.log("onstart");
     };
-    this.manager.onaudiostart = (e: Event) => {
+    this.interface.onaudiostart = (e: Event) => {
       console.log("onaudiostart");
     };
-    this.manager.onsoundstart = (e: Event) => {
+    this.interface.onsoundstart = (e: Event) => {
       console.log("onsoundstart");
     };
-    this.manager.onspeechstart = (e: Event) => {
+    this.interface.onspeechstart = (e: Event) => {
       console.log("onspeechstart");
     };
-    this.manager.onspeechend = (e: Event) => {
+    this.interface.onspeechend = (e: Event) => {
       console.log("onspeechend");
     };
-    this.manager.onsoundend = (e: Event) => {
+    this.interface.onsoundend = (e: Event) => {
       console.log("onsoundend");
     };
-    this.manager.onaudioend = (e: Event) => {
+    this.interface.onaudioend = (e: Event) => {
       console.log("onaudioend");
     };
-    this.manager.onresult = (e: SpeechRecognitionEvent) => {
+    this.interface.onresult = (e: SpeechRecognitionEvent) => {
       console.log("onresult");
     };
-    this.manager.onend = (e: Event) => {
+    this.interface.onend = (e: Event) => {
       console.log("onend");
     };
-    this.manager.onnomatch = (e: SpeechRecognitionEvent) => {
+    this.interface.onnomatch = (e: SpeechRecognitionEvent) => {
       console.log("onnomatch");
     };
-    this.manager.onerror = (e: Event) => {
+    this.interface.onerror = (e: Event) => {
       console.log("onend");
     };
   }
