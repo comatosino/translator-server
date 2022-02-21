0d5c1a186f0b69a4c0e7fc19adbb0611a7e11aaa
Robert Adams <adamsiii.robert@gmail.com>
2022-02-20 17:40:55 -0800

    added useEffect to update interface instance properties

--- src/hooks/useSpeechToText.ts
+++ src/hooks/useSpeechToText.ts
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import useLocalStorage from "./useLocalStorage";
 
 import SpeechToText from "../classes/SpeechToText";
-import SpeechToTextOptions from "../models/speechToTextOptions";
+import SpeechToTextOptions from "../models/SpeechToTextOptions";
 
 const DEFAULTS: SpeechToTextOptions = {
   lang: "",
   continuous: false,
-  interim: false,
+  interimResults: false,
 };
 
 const useSpeechToText = (): [
@@ -16,12 +16,23 @@ const useSpeechToText = (): [
   [SpeechToTextOptions, React.Dispatch<SpeechToTextOptions>]
 ] => {
   const sttIsSupported = SpeechToText.isSupported();
-  const [manager] = useState<SpeechToText>(() => SpeechToText.getInstance());
+
+  const [sst] = useState<SpeechToText>(() => SpeechToText.getInstance());
   const config = useLocalStorage<SpeechToTextOptions>(
     "SpeechToTextOptions",
     DEFAULTS
   );
-  return [sttIsSupported, manager, config];
+
+  // update instance on config
+  useEffect(() => {
+    if (sttIsSupported) {
+      sst.interface.lang = config[0].lang;
+      sst.interface.continuous = config[0].continuous;
+      sst.interface.interimResults = config[0].interimResults;
+    }
+  }, [sttIsSupported, sst, config]);
+
+  return [sttIsSupported, sst, config];
 };
 
 export default useSpeechToText;
