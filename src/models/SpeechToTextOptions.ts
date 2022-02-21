8bdc44687e3e429c58d0c694bb19574ca818bc83
Robert Adams <adamsiii.robert@gmail.com>
2022-02-20 17:18:03 -0800

    renamed property

--- src/models/SpeechToTextOptions.ts
+++ src/models/SpeechToTextOptions.ts
@@ -1,5 +1,5 @@
 export default interface SpeechToTextOptions {
   lang: string;
   continuous: boolean; // continuous listening
-  interim: boolean; // return results as they are processed
+  interimResults: boolean; // return results as they are processed
 }
