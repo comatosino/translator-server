33053446a38c8a2f89129a99ccaf8b4752657f13
Robert Adams <adamsiii.robert@gmail.com>
2022-02-23 18:28:14 -0800

    renamed folders

--- src/models/SpeechToTextOptions.ts
+++
@@ -1,5 +0,0 @@
-export default interface SpeechToTextOptions {
-  lang: string;
-  continuous: boolean; // continuous listening
-  interimResults: boolean; // return results as they are processed
-}
--- src/models/TextToSpeechOptions.ts
+++
@@ -1,6 +0,0 @@
-export default interface TextToSpeechOptions {
-  voice: string;
-  pitch: number | number[]; // range between 0 (lowest) and 2 (highest)
-  rate: number | number[]; // between 0.1 (lowest) and 10 (highest)
-  volume: number | number[]; // between 0 (lowest) and 1 (highest)
-}
