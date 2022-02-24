02e7bbb96ab16b15e34794c922c9d2b16edf45e5
Robert Adams <adamsiii.robert@gmail.com>
2022-02-23 18:59:07 -0800

    implemented translation flow

--- src/Translator.tsx
+++ src/Translator.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import useSpeechToText from "./hooks/useSpeechToText";
 import useTextToSpeech from "./hooks/useTextToSpeech";
@@ -13,58 +13,78 @@ import {
 import Main from "./pages/Main";
 import Options from "./pages/Options";
 
+import API from "./utils/API";
+
 const Translator: React.FC = (): JSX.Element => {
-  const [page, setPage] = useState<number>(0);
-
-  const [speechToTextAvailable, speechToText, useSttOptions] =
+  const { speechToTextAvailable, useSpeechToTextOptions, microphone } =
     useSpeechToText();
+  const { textToSpeechAvailable, useTextToSpeechOptions, speaker, muiLists } =
+    useTextToSpeech();
 
-  const [
-    textToSpeechAvailable,
-    textToSpeech,
-    useTtsOptions,
-    voicesReady,
-    langCodeList,
-    MUIvoiceDisplayList,
-  ] = useTextToSpeech();
+  const [page, setPage] = useState<number>(0);
+  const [translation, setTranslation] = useState<string>("");
 
-  const srcLang = useSttOptions[0].lang;
-  const trgLang = useTtsOptions[0].voice.substring(0, 5);
+  const srcLang = useSpeechToTextOptions[0].lang;
+  const trgLang = useTextToSpeechOptions[0].voice.substring(0, 5);
+
+  const startListening = (): void => {
+    setTranslation("");
+    microphone.listen();
+  };
+
+  const translate = useCallback(
+    async (text: string) => {
+      try {
+        const response = await API.translate(srcLang, trgLang, text);
+        const result = response.data.data.translations[0].translatedText;
+        setTranslation(result);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    [srcLang, trgLang]
+  );
+
+  useEffect(() => {
+    if (microphone.transcript) {
+      translate(microphone.transcript);
+    }
+  }, [microphone.transcript, translate]);
+
+  useEffect(() => {
+    if (translation) {
+      const options = useTextToSpeechOptions[0];
+      speaker.speak(translation, options);
+      setTranslation("");
+    }
+  }, [speaker, translation, useTextToSpeechOptions]);
 
   return speechToTextAvailable && textToSpeechAvailable ? (
     <Container fixed maxWidth="sm">
-      <Paper elevation={5}>
-        {page === 0 ? (
-          <Main
-            textToSpeech={textToSpeech}
-            speechToText={speechToText}
-            src={srcLang}
-            trg={trgLang}
-          />
-        ) : (
-          <Options
-            voicesReady={voicesReady}
-            langCodeList={langCodeList}
-            MUIvoiceDisplayList={MUIvoiceDisplayList}
-            useSttOptions={useSttOptions}
-            useTtsOptions={useTtsOptions}
-          />
-        )}
-        <Paper sx={{ width: "auto" }} elevation={3}>
-          <BottomNavigation
-            showLabels
-            value={page}
-            onChange={(
-              _e: React.SyntheticEvent<Element, Event>,
-              value: number
-            ) => {
-              setPage(value);
-            }}
-          >
-            <BottomNavigationAction label="Main" />
-            <BottomNavigationAction label="Options" />
-          </BottomNavigation>
-        </Paper>
+      {page === 0 ? (
+        <Main startListening={startListening} />
+      ) : (
+        <Options
+          voicesReady={speaker.voicesReady}
+          useSpeechToTextOptions={useSpeechToTextOptions}
+          useTextToSpeechOptions={useTextToSpeechOptions}
+          muiLists={muiLists}
+        />
+      )}
+      <Paper sx={{ width: "auto" }} elevation={3}>
+        <BottomNavigation
+          showLabels
+          value={page}
+          onChange={(
+            _e: React.SyntheticEvent<Element, Event>,
+            value: number
+          ) => {
+            setPage(value);
+          }}
+        >
+          <BottomNavigationAction label="Main" />
+          <BottomNavigationAction label="Options" />
+        </BottomNavigation>
       </Paper>
     </Container>
   ) : (
