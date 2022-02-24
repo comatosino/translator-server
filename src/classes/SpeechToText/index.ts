9237e568ca2e0d75c76c4fe376b8680e62490aa1
Robert Adams <adamsiii.robert@gmail.com>
2022-02-23 18:34:45 -0800

    refactor, moved event handlers to hook

--- src/classes/SpeechToText/index.ts
+++ src/classes/SpeechToText/index.ts
@@ -1,44 +1,12 @@
-const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
-
 export default class SpeechToText {
   private static _instance: SpeechToText;
   public interface: SpeechRecognition;
+  public listening: boolean = false;
+  public transcript: string = "";
 
   private constructor() {
-    this.interface = new SpeechRecognition() || new webkitSpeechRecognition();
-    this.interface.onstart = (e: Event) => {
-      console.log("onstart");
-    };
-    this.interface.onaudiostart = (e: Event) => {
-      console.log("onaudiostart");
-    };
-    this.interface.onsoundstart = (e: Event) => {
-      console.log("onsoundstart");
-    };
-    this.interface.onspeechstart = (e: Event) => {
-      console.log("onspeechstart");
-    };
-    this.interface.onspeechend = (e: Event) => {
-      console.log("onspeechend");
-    };
-    this.interface.onsoundend = (e: Event) => {
-      console.log("onsoundend");
-    };
-    this.interface.onaudioend = (e: Event) => {
-      console.log("onaudioend");
-    };
-    this.interface.onresult = (e: SpeechRecognitionEvent) => {
-      console.log("onresult");
-    };
-    this.interface.onend = (e: Event) => {
-      console.log("onend");
-    };
-    this.interface.onnomatch = (e: SpeechRecognitionEvent) => {
-      console.log("onnomatch");
-    };
-    this.interface.onerror = (e: Event) => {
-      console.log("onend");
-    };
+    const Recognition = webkitSpeechRecognition || SpeechRecognition;
+    this.interface = new Recognition();
   }
 
   static isSupported(): boolean {
@@ -49,5 +17,50 @@ export default class SpeechToText {
     if (this._instance) return this._instance;
     this._instance = new SpeechToText();
     return this._instance;
+  }
+
+  get lang() {
+    return this.interface.lang;
+  }
+
+  set lang(lang: string) {
+    this.interface.lang = lang;
+  }
+
+  get continuous() {
+    return this.interface.continuous;
+  }
+
+  set continuous(bool: boolean) {
+    this.interface.continuous = bool;
+  }
+
+  get interimResults() {
+    return this.interface.interimResults;
+  }
+
+  set interimResults(bool: boolean) {
+    this.interface.interimResults = bool;
+  }
+
+  start() {
+    this.clear();
+    this.listening = true;
+    this.interface.start();
+  }
+
+  stop() {
+    this.listening = false;
+    this.interface.stop();
+  }
+
+  abort() {
+    this.clear();
+    this.listening = false;
+    this.interface.abort();
+  }
+
+  clear() {
+    this.transcript = "";
   }
 }
