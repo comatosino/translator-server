2ee64940b858e45baeb29053cd6ff5ecb76b1b23
Robert Adams <adamsiii.robert@gmail.com>
2022-02-25 12:42:21 -0800

    added conditional; no http req if srcLang = trgLang

--- src/utils/API.ts
+++ src/utils/API.ts
@@ -3,25 +3,27 @@ import { GOOGLE_MAPS_API_KEY } from "./env";
 
 export default class API {
   static translate = (srcLang: string, trgLang: string, text: string) => {
-    const url = `https://translation.googleapis.com/language/translate/v2`;
-    const reqConfig: AxiosRequestConfig = {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-      },
-      params: {
-        key: `${GOOGLE_MAPS_API_KEY}`,
-      },
-    };
+    if (srcLang !== trgLang) {
+      const url = `https://translation.googleapis.com/language/translate/v2`;
+      const reqConfig: AxiosRequestConfig = {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+        },
+        params: {
+          key: `${GOOGLE_MAPS_API_KEY}`,
+        },
+      };
 
-    return axios.post(
-      url,
-      {
-        q: text,
-        source: srcLang.substring(0, 2),
-        target: trgLang.substring(0, 2),
-      },
-      reqConfig
-    );
+      return axios.post(
+        url,
+        {
+          q: text,
+          source: srcLang.substring(0, 2),
+          target: trgLang.substring(0, 2),
+        },
+        reqConfig
+      );
+    }
   };
 }
