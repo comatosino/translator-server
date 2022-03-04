8bc6cd07229351e4a0516a1ebdc1ada07b10b3c5
Robert Adams <adamsiii.robert@gmail.com>
2022-03-03 16:09:02 -0800

    modified client api fetch url

--- client/src/utils/API.ts
+++ client/src/utils/API.ts
@@ -1,28 +1,24 @@
-import axios, { AxiosRequestConfig } from "axios";
+import axios from "axios";
 
 export default class API {
   static translate = (srcLang: string, trgLang: string, text: string) => {
-    if (srcLang !== trgLang) {
-      const url = `https://translation.googleapis.com/language/translate/v2`;
-      const reqConfig: AxiosRequestConfig = {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-        },
-        params: {
-          key: `${process.env.GOOGLE_MAPS_API_KEY}`,
-        },
-      };
+    const url = `/api/translate`;
+    const body = { srcLang, trgLang, text };
+    return axios.post(url, body);
+  };
 
-      return axios.post(
-        url,
-        {
-          q: text,
-          source: srcLang.substring(0, 2),
-          target: trgLang.substring(0, 2),
-        },
-        reqConfig
-      );
-    }
+  static signup = () => {
+    const url = `/auth/signup`;
+    return axios.post(url);
+  };
+
+  static signin = () => {
+    const url = `/auth/signin`;
+    return axios.post(url);
+  };
+
+  static signout = () => {
+    const url = `/auth/signout`;
+    return axios.post(url);
   };
 }
