7ea3c491b1d487035434ab2467627c72084f1d89
Robert Adams <adamsiii.robert@gmail.com>
2022-03-03 16:06:01 -0800

    imported routes

--- server.ts
+++ server.ts
@@ -1,23 +1,17 @@
 import "dotenv/config";
 import express from "express";
 import path from "path";
-
-console.log(process.env);
+import routes from "./routes";
 
 const PORT = process.env.PORT || 5000;
-
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("../client/build")); // Serve the React static files after build
+app.use(express.static("../client/build"));
+app.use(routes);
 
-app.get("/api/hello", (req, res) => {
-  res.send({ message: "Hello" });
-});
-
-// All other unmatched requests will return the React app
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
