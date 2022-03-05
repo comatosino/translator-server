6a2290fdd2f57d796b61ed70004d8f890e82aa9b
Robert Adams <adamsiii.robert@gmail.com>
2022-03-05 09:40:51 -0800

    removed debug statements

--- controllers/api.ts
+++ controllers/api.ts
@@ -3,35 +3,37 @@ import { RequestHandler } from "express";
 import axios, { AxiosRequestConfig } from "axios";
 
 export const translate: RequestHandler = async (req, res) => {
-  const { srcLang, trgLang, text } = req.body;
+  try {
+    const { srcLang, trgLang, text } = req.body;
 
-  const source = srcLang.substring(0, 2);
-  const target = trgLang.substring(0, 2);
+    const source = srcLang.substring(0, 2);
+    const target = trgLang.substring(0, 2);
 
-  console.log(source);
-  console.log(target);
-  console.log(text);
+    if (srcLang !== trgLang) {
+      const url = `https://translation.googleapis.com/language/translate/v2`;
+      const reqConfig: AxiosRequestConfig = {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+        },
+        params: {
+          key: `${process.env.GOOGLE_MAPS_API_KEY}`,
+        },
+      };
 
-  if (srcLang !== trgLang) {
-    const url = `https://translation.googleapis.com/language/translate/v2`;
-    const reqConfig: AxiosRequestConfig = {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-      },
-      params: {
-        key: `${process.env.GOOGLE_MAPS_API_KEY}`,
-      },
-    };
+      const response = await axios.post(
+        url,
+        {
+          q: text,
+          source,
+          target,
+        },
+        reqConfig
+      );
 
-    return await axios.post(
-      url,
-      {
-        q: text,
-        source,
-        target,
-      },
-      reqConfig
-    );
+      return response;
+    }
+  } catch (error) {
+    console.log(error);
   }
 };
