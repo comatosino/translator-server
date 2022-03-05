11c5bf16980358148dd38c55e158624af0f9fd09
Robert Adams <adamsiii.robert@gmail.com>
2022-03-05 11:44:14 -0800

    added GET /auth/user route

--- routes/auth/index.ts
+++ routes/auth/index.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { login, register } from "../../controllers/auth";
+import { getUser, login, register, logout } from "../../controllers/auth";
 
 const router = Router();
 
+router.get("/user", getUser);
+
 router.post("/login", login);
 router.post("/register", register);
+
+router.delete("/logout", logout);
 
 export default router;
