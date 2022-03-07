2c3b6be99cb44e8fb131a3630ebf30ae075664f4
Robert Adams <adamsiii.robert@gmail.com>
2022-03-07 14:41:38 -0800

    added action creators

---
+++ client/src/store/user/actions.ts
@@ -0,0 +1,36 @@
+import { User } from "../../types/User";
+import { USER_STATE_DEFAULTS } from "../../utils/defaultOptions";
+
+export type Action = {
+  type: string;
+  payload: unknown;
+};
+
+// ACTION TYPES
+
+export const SET_USER = "GET_USER";
+export const SET_FETCHING_STATUS = "SET_FETCHING_STATUS";
+export const CLEAR_USER = "CLEAR_USER";
+
+// ACTION CREATORS
+
+export const set_User = (user: User): Action => {
+  return {
+    type: SET_USER,
+    payload: user,
+  };
+};
+
+export const set_Fetching_Status = (isFetching: boolean): Action => {
+  return {
+    type: SET_FETCHING_STATUS,
+    payload: isFetching,
+  };
+};
+
+export const clear_User = (): Action => {
+  return {
+    type: CLEAR_USER,
+    payload: USER_STATE_DEFAULTS,
+  };
+};
