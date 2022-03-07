ee0c247cc261886aaaba2feeb2c7f8d4bc9f27ab
Robert Adams <adamsiii.robert@gmail.com>
2022-03-07 14:32:17 -0800

    removed unused routes

--- routes/auth/index.ts
+++ routes/auth/index.ts
@@ -1,13 +1,9 @@
 import { Router } from "express";
-import { getUser, login, register, logout } from "../../controllers/auth";
+import { login, register } from "../../controllers/auth";
 
 const router = Router();
 
-router.get("/user", getUser);
-
 router.post("/login", login);
 router.post("/register", register);
-
-router.delete("/logout", logout);
 
 export default router;
