47c9d80ef73e1c5b7422b015388c592bfe092e55
Robert Adams <adamsiii.robert@gmail.com>
2022-03-10 12:51:11 -0800

    moved files

--- client/src/types/SpeechToTextOptions.ts
+++
@@ -1,7 +0,0 @@
-type SpeechToTextOptions = {
-  lang: string;
-  continuous: boolean; // keep listening until user manually ends transcription
-  interimResults: boolean; // return results before they are finalized
-};
-
-export default SpeechToTextOptions;
