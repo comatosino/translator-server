a63ceb5be90dc05acd71ea0f234acfc93fd20849
Robert Adams <adamsiii.robert@gmail.com>
2022-03-10 12:55:14 -0800

    added reducer functions

---
+++ client/src/hooks/useSpeechToText/store/reducerFunctions.ts
@@ -0,0 +1,34 @@
+import { Reducer, ReducerWithoutAction } from "react";
+import { SpeechToTextState } from "../types";
+
+export const __setTranscript: Reducer<SpeechToTextState, string> = (
+  state,
+  payload
+): SpeechToTextState => ({ ...state, transcript: payload });
+
+export const __clearTranscript: ReducerWithoutAction<SpeechToTextState> = (
+  state
+): SpeechToTextState => ({ ...state, transcript: "" });
+
+export const __setLang: Reducer<SpeechToTextState, string> = (
+  state,
+  payload
+): SpeechToTextState => ({ ...state, language: payload });
+
+export const __setListening: Reducer<SpeechToTextState, boolean> = (
+  state,
+  payload
+): SpeechToTextState => ({ ...state, listening: payload });
+
+export const __setContinuous: Reducer<SpeechToTextState, boolean> = (
+  state,
+  payload
+): SpeechToTextState => ({ ...state, continuous: payload });
+
+export const __setInterimResults: Reducer<SpeechToTextState, boolean> = (
+  state,
+  payload
+): SpeechToTextState => ({
+  ...state,
+  interimResults: payload,
+});
