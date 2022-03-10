2c9f7e793e3d961b8957792c889c228ba49b72ff
Robert Adams <adamsiii.robert@gmail.com>
2022-03-10 12:54:23 -0800

    moved file

---
+++ client/src/hooks/useSpeechToText/SpeechToText.ts
@@ -0,0 +1,66 @@
+export default class SpeechToText {
+  private static _instance: SpeechToText;
+  public interface: SpeechRecognition;
+  public listening: boolean = false;
+  public transcript: string = "";
+
+  private constructor() {
+    const Recognition = webkitSpeechRecognition || SpeechRecognition;
+    this.interface = new Recognition();
+  }
+
+  static isSupported(): boolean {
+    return "SpeechRecognition" in window || "webkitSpeechRecognition" in window;
+  }
+
+  static getInstance(): SpeechToText {
+    if (this._instance) return this._instance;
+    this._instance = new SpeechToText();
+    return this._instance;
+  }
+
+  get lang() {
+    return this.interface.lang;
+  }
+
+  set lang(lang: string) {
+    this.interface.lang = lang;
+  }
+
+  get continuous() {
+    return this.interface.continuous;
+  }
+
+  set continuous(bool: boolean) {
+    this.interface.continuous = bool;
+  }
+
+  get interimResults() {
+    return this.interface.interimResults;
+  }
+
+  set interimResults(bool: boolean) {
+    this.interface.interimResults = bool;
+  }
+
+  start() {
+    this.clearTranscript();
+    this.listening = true;
+    this.interface.start();
+  }
+
+  stop() {
+    this.listening = false;
+    this.interface.stop();
+  }
+
+  abort() {
+    this.clearTranscript();
+    this.listening = false;
+    this.interface.abort();
+  }
+
+  clearTranscript() {
+    this.transcript = "";
+  }
+}
