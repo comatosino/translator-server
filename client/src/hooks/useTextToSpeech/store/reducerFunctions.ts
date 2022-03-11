35ca24ab6df030d5d9a258138060bc6ae1c5cc75
Robert Adams <adamsiii.robert@gmail.com>
2022-03-10 16:30:52 -0800

    added reducer functions

---
+++ client/src/hooks/useTextToSpeech/store/reducerFunctions.ts
@@ -0,0 +1,44 @@
+import { Reducer } from "react";
+import { TextToSpeechState, SpeechSynthesisVoiceMap } from "../types";
+
+export const __setSpeaking: Reducer<TextToSpeechState, boolean> = (
+  state,
+  payload
+): TextToSpeechState => {
+  return { ...state, speaking: payload };
+};
+
+export const __setSelectedVoice: Reducer<
+  TextToSpeechState,
+  SpeechSynthesisVoice
+> = (state, payload): TextToSpeechState => {
+  return { ...state, selectedVoice: payload };
+};
+
+export const __setVoices: Reducer<
+  TextToSpeechState,
+  SpeechSynthesisVoiceMap
+> = (state, payload): TextToSpeechState => {
+  return { ...state, voices: payload };
+};
+
+export const __setVolume: Reducer<TextToSpeechState, number> = (
+  state,
+  payload
+): TextToSpeechState => {
+  return { ...state, volume: payload };
+};
+
+export const __setRate: Reducer<TextToSpeechState, number> = (
+  state,
+  payload
+): TextToSpeechState => {
+  return { ...state, rate: payload };
+};
+
+export const __setPitch: Reducer<TextToSpeechState, number> = (
+  state,
+  payload
+): TextToSpeechState => {
+  return { ...state, pitch: payload };
+};
