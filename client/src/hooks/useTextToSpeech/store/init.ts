ac090e390e28849e1ad7a770641be2688b75bcaf
Robert Adams <adamsiii.robert@gmail.com>
2022-03-10 16:29:50 -0800

    moved initial state into own file

---
+++ client/src/hooks/useTextToSpeech/store/init.ts
@@ -0,0 +1,14 @@
+import TextToSpeech from "../TextToSpeech";
+import { TextToSpeechState } from "../types";
+
+const INITIAL_STATE: TextToSpeechState = {
+  textToSpeech: TextToSpeech.getInstance(),
+  speaking: false,
+  selectedVoice: null,
+  voices: null,
+  volume: 1,
+  rate: 1,
+  pitch: 1,
+};
+
+export default INITIAL_STATE;
