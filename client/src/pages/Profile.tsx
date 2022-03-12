3fc19c071d1d7d41ea163a8a5cfaaf7358254f22
Robert Adams <adamsiii.robert@gmail.com>
2022-03-11 19:53:00 -0800

    removed files

--- client/src/pages/Profile.tsx
+++
@@ -1,9 +0,0 @@
-import { useAppSelector } from "../store/hooks";
-import { Translator, Auth } from ".";
-
-const Profile = () => {
-  const userProfile = useAppSelector((state) => state.user.profile);
-  return userProfile ? <Translator user={userProfile} /> : <Auth />;
-};
-
-export default Profile;
--- client/src/pages/index.ts
+++
@@ -1,7 +0,0 @@
-import Loading from "./Loading";
-import Auth from "./Auth";
-import Translator from "./Translator";
-import Options from "./Options";
-import Profile from "./Profile";
-
-export { Loading, Auth, Translator, Options, Profile };
