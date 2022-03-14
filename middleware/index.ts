f0073b3491220f5eaa732ce03a38c9def5779ff8
Robert Adams <adamsiii.robert@gmail.com>
2022-03-14 11:08:42 -0700

    added index file and moved authenticate function to own named file

--- middleware/index.ts
+++ middleware/index.ts
@@ -1,22 +1,3 @@
-import jwt from "jsonwebtoken";
-import { RequestHandler } from "express";
+import authenticate from "./authenticate";
 
-const SECRET_KEY = process.env.SECRET_KEY!;
-
-export const authenticate: RequestHandler = (req, _res, next) => {
-  const bearer = req.headers.authorization;
-  const token = bearer?.split(" ")[1];
-
-  if (token) {
-    jwt.verify(token, SECRET_KEY, (err, payload: any) => {
-      if (err) {
-        next(err);
-      } else {
-        req.userID = payload.userID; // TODO: add typing so payload accepts userID property
-        next();
-      }
-    });
-  } else {
-    next();
-  }
-};
+export { authenticate };
