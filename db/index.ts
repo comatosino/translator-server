4d4dcc459d132169f732460d027adaf48b05c0c5
Robert Adams <adamsiii.robert@gmail.com>
2022-03-14 13:45:36 -0700

    created simple export object to connect / disconnect from Mongo

---
+++ db/index.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const uri = process.env.MONGODB_URI || "mongodb://localhost/translatordb";
+
+const db = {
+  connect: async function () {
+    try {
+      await mongoose.connect(uri);
+    } catch (error) {
+      console.error(error);
+      this.disconnect();
+    }
+  },
+  disconnect: async function () {
+    try {
+      await mongoose.disconnect();
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
+
+export default db;
