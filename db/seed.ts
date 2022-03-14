d17e0789d84d72698042e75f0aa4508839088dc1
Robert Adams <adamsiii.robert@gmail.com>
2022-03-14 13:46:26 -0700

    added simple seed file

---
+++ db/seed.ts
@@ -0,0 +1,29 @@
+import db from ".";
+import { User, Translation } from "./models";
+
+const addData = async () => {
+  try {
+    const testUser = await User.create({
+      username: "Rob",
+      password: "passweird",
+      translations: [],
+    });
+    const testTranslation = await Translation.create({
+      srcLang: "en-US",
+      srcText: "hello",
+      trgLang: "es-US",
+      trgText: "hola",
+    });
+  } catch (error) {
+    console.error("ERROR SEEDING");
+    console.error(error);
+  }
+};
+
+const seed = async () => {
+  await db.connect();
+  await addData();
+  await db.disconnect();
+};
+
+seed();
