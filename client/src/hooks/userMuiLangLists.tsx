4725613264cf0d55c50e6d343470220cfe3348ae
Robert Adams <adamsiii.robert@gmail.com>
2022-03-15 12:14:28 -0700

    commented out unused list

--- client/src/hooks/userMuiLangLists.tsx
+++ client/src/hooks/userMuiLangLists.tsx
@@ -3,12 +3,12 @@ import { Speaker } from "./useTextToSpeech/types";
 
 export type MuiLists = {
   srcCodeList: string[];
-  trgCodeList: { type: string; content: string; lang: string }[];
+  // trgCodeList: { type: string; content: string; lang: string }[];
 };
 
 const useMuiLangLists = (
   speaker: Speaker
-): [MuiLists["srcCodeList"], MuiLists["trgCodeList"]] => {
+): [MuiLists["srcCodeList"]] => {
   // source language select options
   // creates a list of BCP 47 language tags for display
   const srcCodeList = useMemo<MuiLists["srcCodeList"]>(() => {
@@ -24,29 +24,29 @@ const useMuiLangLists = (
 
   // target language & voice select options
   // creates list of objects with info for MUI MenuItem & Subheader components
-  const trgCodeList = useMemo<MuiLists["trgCodeList"]>(() => {
-    const voiceArr = speaker.getVoiceArray();
+  // const trgCodeList = useMemo<MuiLists["trgCodeList"]>(() => {
+  //   const voiceArr = speaker.getVoiceArray();
 
-    let result: MuiLists["trgCodeList"] = [];
-    if (voiceArr.length) {
-      const voiceMap = speaker.getVoiceMap();
-      const langList = Object.keys(voiceMap!);
+  //   let result: MuiLists["trgCodeList"] = [];
+  //   if (voiceArr.length) {
+  //     const voiceMap = speaker.getVoiceMap();
+  //     const langList = Object.keys(voiceMap!);
 
-      for (let i = 0; i < langList.length; i++) {
-        const langCode = langList[i];
-        const langVoices = voiceMap![langCode];
-        result.push({ type: "subheader", content: langCode, lang: langCode });
+  //     for (let i = 0; i < langList.length; i++) {
+  //       const langCode = langList[i];
+  //       const langVoices = voiceMap![langCode];
+  //       result.push({ type: "subheader", content: langCode, lang: langCode });
 
-        for (let j = 0; j < langVoices.length; j++) {
-          const voice = langVoices[j];
-          result.push({ type: "item", content: voice.name, lang: voice.lang });
-        }
-      }
-    }
-    return result;
-  }, [speaker]);
+  //       for (let j = 0; j < langVoices.length; j++) {
+  //         const voice = langVoices[j];
+  //         result.push({ type: "item", content: voice.name, lang: voice.lang });
+  //       }
+  //     }
+  //   }
+  //   return result;
+  // }, [speaker]);
 
-  return [srcCodeList, trgCodeList];
+  return [srcCodeList];
 };
 
 export default useMuiLangLists;
