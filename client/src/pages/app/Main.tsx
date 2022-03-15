d8bc0bcdde3e353c22a5e36c52baf026c4a5cad2
Robert Adams <adamsiii.robert@gmail.com>
2022-03-15 10:17:53 -0700

    updated main page to include flag svgs for lang selects

--- client/src/pages/app/Main.tsx
+++ client/src/pages/app/Main.tsx
@@ -1,93 +1,142 @@
 import { Microphone } from "../../hooks/useSpeechToText/types";
 import { Speaker } from "../../hooks/useTextToSpeech/types";
-import { MuiLists } from "../../hooks/userMuiLangLists";
 import {
   Box,
   FormControl,
   InputLabel,
-  ListSubheader,
   MenuItem,
   Select,
   SelectChangeEvent,
+  IconButton,
   Stack,
+  FormHelperText,
 } from "@mui/material";
+import MicNoneIcon from "@mui/icons-material/MicNone";
+import {
+  setLanguage,
+  setSelectedVoice,
+} from "../../hooks/useTextToSpeech/store/actions";
 
 const Main: React.FC<{
   microphone: Microphone;
   speaker: Speaker;
-  srcCodeList: MuiLists["srcCodeList"];
-  trgCodeList: MuiLists["trgCodeList"];
-}> = ({ microphone, speaker, srcCodeList, trgCodeList }): JSX.Element => {
+  langCodes: string[];
+}> = ({ microphone, speaker, langCodes }): JSX.Element => {
+  const handleSetSourceLang = (e: SelectChangeEvent<string>) => {
+    microphone.setLanguage(e.target.value);
+  };
+
+  const handleSetTargetLang = (e: SelectChangeEvent<string>) => {
+    const voices = speaker.getVoiceMap();
+    speaker.dispatch(setLanguage(e.target.value));
+    if (voices) {
+      const defaultVoice = voices[e.target.value][0];
+      speaker.dispatch(setSelectedVoice(defaultVoice));
+    }
+  };
+
   return (
     <Box
       sx={{
-        height: 1,
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <Stack spacing={3}>
-        <FormControl fullWidth>
+      <Stack
+        sx={{ display: "flex", alignItems: "center" }}
+        direction="row"
+        spacing={10}
+      >
+        <FormControl variant="standard">
           <InputLabel id="src-lang">Source Language</InputLabel>
           <Select
             label="Source Language"
             id="src-lang"
             name="source"
-            value={microphone.language || srcCodeList[0] || ""}
-            onChange={(e: SelectChangeEvent<string>) => {
-              microphone.setLanguage(e.target.value);
-            }}
+            value={microphone.language || ""}
+            onChange={handleSetSourceLang}
           >
-            <MenuItem value="">
-              <em>Select a language!</em>
-            </MenuItem>
-            {srcCodeList.map((lang) => (
-              <MenuItem key={lang} value={lang}>
-                {lang}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+            {langCodes.map((code) => {
+              const [lang, country] = code.split("-");
 
-        <FormControl fullWidth>
-          <InputLabel id="tgt-lang">Target Language</InputLabel>
-          <Select
-            label="Target Language"
-            id="tgt-lang"
-            name="target"
-            value={speaker.selectedVoice?.name || ""}
-            onChange={({ target }: SelectChangeEvent<string>) => {
-              const voiceName = target.value;
-              const voiceArr = speaker.getVoiceArray()!;
-              const voice = voiceArr.find((voice) => voice.name === voiceName);
-              if (voice) speaker.setSelectedVoice(voice);
-            }}
-          >
-            <MenuItem value="">
-              <em>Select a language and voice!</em>
-            </MenuItem>
-
-            {trgCodeList.map((data) => {
-              if (data.type === "subheader")
+              if (lang === "es" && country === "US") {
                 return (
-                  <ListSubheader key={data.content}>{data.lang}</ListSubheader>
-                );
-              if (data.type === "item")
-                return (
-                  <MenuItem key={data.content} value={data.content}>
-                    {data.content}
+                  <MenuItem key={code} value={code}>
+                    <img
+                      loading="lazy"
+                      width="100"
+                      src={`https://flagcdn.com/mx.svg`}
+                      alt={""}
+                    />
                   </MenuItem>
                 );
+              }
+
               return (
-                <MenuItem value="">
-                  <em></em>
+                <MenuItem key={code} value={code}>
+                  <img
+                    loading="lazy"
+                    width="100"
+                    src={`https://flagcdn.com/${country.toLowerCase()}.svg`}
+                    alt={""}
+                  />
                 </MenuItem>
               );
             })}
           </Select>
+          <FormHelperText>{microphone.language}</FormHelperText>
+        </FormControl>
+
+        <FormControl variant="standard">
+          <InputLabel id="src-lang">Target Language</InputLabel>
+          <Select
+            label="Target Language"
+            id="trg-lang"
+            name="target"
+            value={speaker.language || ""}
+            onChange={handleSetTargetLang}
+          >
+            {langCodes.map((code) => {
+              const [lang, country] = code.split("-");
+
+              if (lang === "es" && country === "US") {
+                return (
+                  <MenuItem key={code} value={code}>
+                    <img
+                      loading="lazy"
+                      width="100"
+                      src={`https://flagcdn.com/mx.svg`}
+                      alt={""}
+                    />
+                  </MenuItem>
+                );
+              }
+              return (
+                <MenuItem key={code} value={code}>
+                  <img
+                    loading="lazy"
+                    width="100"
+                    src={`https://flagcdn.com/${country.toLowerCase()}.svg`}
+                    alt={""}
+                  />
+                </MenuItem>
+              );
+            })}
+          </Select>
+          <FormHelperText>{speaker.language}</FormHelperText>
         </FormControl>
       </Stack>
+
+      <IconButton
+        sx={{
+          position: "fixed",
+          bottom: 100,
+        }}
+        aria-label="microphone"
+      >
+        <MicNoneIcon sx={{ fontSize: "5em" }} />
+      </IconButton>
     </Box>
   );
 };
