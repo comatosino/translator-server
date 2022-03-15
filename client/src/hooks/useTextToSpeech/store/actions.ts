a3285180b13ad0ad1ce71f8baae781eee30fa544
Robert Adams <adamsiii.robert@gmail.com>
2022-03-15 10:12:43 -0700

    added setLanguage action creator

--- client/src/hooks/useTextToSpeech/store/actions.ts
+++ client/src/hooks/useTextToSpeech/store/actions.ts
@@ -1,15 +1,17 @@
 import {
   TextToSpeechActions,
-  SpeechSynthesisVoiceMap,
+  StringPayloadAction,
   VoicesPayloadAction,
-  SelectedVoicePayloadAction,
   NumberPayloadAction,
   BooleanPayloadAction,
+  SelectedVoicePayloadAction,
+  SpeechSynthesisVoiceMap,
 } from "../types";
 
 export const setVoices = (
   voices: SpeechSynthesisVoiceMap
 ): VoicesPayloadAction => {
+  console.log("SET VOICES", voices);
   return {
     type: TextToSpeechActions.SET_VOICES,
     payload: voices,
@@ -19,13 +21,23 @@ export const setVoices = (
 export const setSelectedVoice = (
   voice: SpeechSynthesisVoice
 ): SelectedVoicePayloadAction => {
+  console.log("SET SELECTED VOICE", voice);
   return {
     type: TextToSpeechActions.SET_SELECTED_VOICE,
     payload: voice,
   };
 };
 
+export const setLanguage = (lang: string): StringPayloadAction => {
+  console.log("SET TARGET LANGUAGE", lang);
+  return {
+    type: TextToSpeechActions.SET_LANGUAGE,
+    payload: lang,
+  };
+};
+
 export const setVolume = (volume: number): NumberPayloadAction => {
+  console.log("SET VOLUME", volume);
   return {
     type: TextToSpeechActions.SET_VOLUME,
     payload: volume,
@@ -33,6 +45,7 @@ export const setVolume = (volume: number): NumberPayloadAction => {
 };
 
 export const setRate = (rate: number): NumberPayloadAction => {
+  console.log("SET RATE", rate);
   return {
     type: TextToSpeechActions.SET_RATE,
     payload: rate,
@@ -40,6 +53,7 @@ export const setRate = (rate: number): NumberPayloadAction => {
 };
 
 export const setPitch = (pitch: number): NumberPayloadAction => {
+  console.log("SET PITCH", pitch);
   return {
     type: TextToSpeechActions.SET_PITCH,
     payload: pitch,
@@ -47,6 +61,7 @@ export const setPitch = (pitch: number): NumberPayloadAction => {
 };
 
 export const setSpeaking = (speaking: boolean): BooleanPayloadAction => {
+  console.log("SET SPEAKING", speaking);
   return {
     type: TextToSpeechActions.SET_SPEAKING,
     payload: speaking,
