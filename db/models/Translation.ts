b479e4eb36420273b286e97432c0731a4a2bddcf
Robert Adams <adamsiii.robert@gmail.com>
2022-03-16 12:34:15 -0700

    added timestamps to models

--- db/models/Translation.ts
+++ db/models/Translation.ts
@@ -5,26 +5,31 @@ interface Translation {
   sourceText: string;
   target: string;
   targetText: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
-const TranslationSchema = new Schema<Translation>({
-  source: {
-    type: String,
-    required: true,
+const TranslationSchema = new Schema<Translation>(
+  {
+    source: {
+      type: String,
+      required: true,
+    },
+    sourceText: {
+      type: String,
+      required: true,
+    },
+    target: {
+      type: String,
+      required: true,
+    },
+    targetText: {
+      type: String,
+      required: true,
+    },
   },
-  sourceText: {
-    type: String,
-    required: true,
-  },
-  target: {
-    type: String,
-    required: true,
-  },
-  targetText: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const Translation = model("Translation", TranslationSchema);
 export default Translation;
