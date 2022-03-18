a5113d2ab7b9dbdc2ffb8e92277c3361cebdb0b7
Robert Adams <adamsiii.robert@gmail.com>
2022-03-17 21:00:52 -0700

    removed loading page

--- client/src/pages/app/index.tsx
+++ client/src/pages/app/index.tsx
@@ -1,6 +1,5 @@
-import Loading from "./Loading";
 import Main from "./Main";
 import Options from "./Options";
 import Record from "./Record";
 
-export { Loading, Main, Options, Record };
+export { Main, Options, Record };
