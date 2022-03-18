2b595c3ea860431dbcf8d4d41b8bd1aad830eb6b
Robert Adams <adamsiii.robert@gmail.com>
2022-03-18 14:08:14 -0700

    added footer component

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,48 @@
+import { Fab } from "@mui/material";
+import { Page } from "../pages/app/Translator";
+import HistoryIcon from "@mui/icons-material/History";
+import MicNoneIcon from "@mui/icons-material/MicNone";
+
+type FooterProps = {
+  page: Page;
+  setPage: React.Dispatch<React.SetStateAction<Page>>;
+};
+
+const Footer: React.FC<FooterProps> = ({ page, setPage }): JSX.Element => {
+  return (
+    <footer style={{ position: "fixed" }}>
+      <nav
+        style={{
+          height: "10vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-evenly",
+        }}
+      >
+        {/* GOTO HISTORY */}
+        {page === Page.MAIN && (
+          <Fab
+            onClick={() => setPage(Page.HISTORY)}
+            color="primary"
+            aria-label="mic"
+          >
+            <HistoryIcon />
+          </Fab>
+        )}
+
+        {/* GOTO MICROPHONE */}
+        {(page === Page.HISTORY || page === Page.OPTIONS) && (
+          <Fab
+            onClick={() => setPage(Page.MAIN)}
+            color="primary"
+            aria-label="mic"
+          >
+            <MicNoneIcon />
+          </Fab>
+        )}
+      </nav>
+    </footer>
+  );
+};
+
+export default Footer;
