c45d8a626a1e9149ba7af43771b7d5cab06ae47c
Robert Adams <adamsiii.robert@gmail.com>
2022-03-19 12:19:00 -0700

    added Box wrapper to apply bg color

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,8 +3,9 @@ import MenuIcon from "@mui/icons-material/Menu";
 import TuneIcon from "@mui/icons-material/Tune";
 import HistoryIcon from "@mui/icons-material/History";
 import MicNoneIcon from "@mui/icons-material/MicNone";
-import { SpeedDial, SpeedDialAction } from "@mui/material";
+import { Box, SpeedDial, SpeedDialAction } from "@mui/material";
 import { Page } from "../pages/app/Translator";
+import theme from "../themes/theme";
 
 type HeaderProps = {
   handleLogout: () => void;
@@ -14,43 +15,45 @@ type HeaderProps = {
 const Header: React.FC<HeaderProps> = (props): JSX.Element => {
   const { setPage, handleLogout } = props;
   return (
-    <header>
-      <nav
-        style={{
-          height: "10vh",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <SpeedDial
-          ariaLabel="speed-dial-menu"
-          direction="right"
-          icon={<MenuIcon />}
+    <Box sx={{ bgcolor: theme.palette.secondary.main }}>
+      <header>
+        <nav
+          style={{
+            height: "10vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
         >
-          <SpeedDialAction
-            onClick={() => setPage(Page.MAIN)}
-            icon={<MicNoneIcon />}
-            tooltipTitle={"Microphone"}
-          />
-          <SpeedDialAction
-            onClick={() => setPage(Page.HISTORY)}
-            icon={<HistoryIcon />}
-            tooltipTitle={"History"}
-          />
-          <SpeedDialAction
-            onClick={() => setPage(Page.OPTIONS)}
-            icon={<TuneIcon />}
-            tooltipTitle={"Options"}
-          />
-          <SpeedDialAction
-            onClick={handleLogout}
-            icon={<LogoutIcon />}
-            tooltipTitle={"Logout"}
-          />
-        </SpeedDial>
-      </nav>
-    </header>
+          <SpeedDial
+            ariaLabel="speed-dial-menu"
+            direction="right"
+            icon={<MenuIcon />}
+          >
+            <SpeedDialAction
+              onClick={() => setPage(Page.MAIN)}
+              icon={<MicNoneIcon />}
+              tooltipTitle={"Microphone"}
+            />
+            <SpeedDialAction
+              onClick={() => setPage(Page.HISTORY)}
+              icon={<HistoryIcon />}
+              tooltipTitle={"History"}
+            />
+            <SpeedDialAction
+              onClick={() => setPage(Page.OPTIONS)}
+              icon={<TuneIcon />}
+              tooltipTitle={"Options"}
+            />
+            <SpeedDialAction
+              onClick={handleLogout}
+              icon={<LogoutIcon />}
+              tooltipTitle={"Logout"}
+            />
+          </SpeedDial>
+        </nav>
+      </header>
+    </Box>
   );
 };
 
