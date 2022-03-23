79b3ac3708ca195357b64a6f438bd81c6ff7d8ec
Robert Adams <adamsiii.robert@gmail.com>
2022-03-22 18:09:02 -0700

    defined theme primary and secondary colors

--- client/src/themes/theme.ts
+++ client/src/themes/theme.ts
@@ -3,6 +3,12 @@ import createTheme from "@mui/material/styles/createTheme";
 const theme = createTheme({
   palette: {
     mode: "dark",
+    primary: {
+      main: "#0066cc",
+    },
+    secondary: {
+      main: "#ffcc00",
+    },
   },
 });
 
