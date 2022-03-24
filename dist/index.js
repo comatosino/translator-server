dc4c1e7b7a66336236b2776f6d343ed9d69e8a8d
Robert Adams <adamsiii.robert@gmail.com>
2022-03-24 12:33:18 -0700

    adjusted seed scripts

--- dist/index.js
+++ dist/index.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
 const db_1 = __importDefault(require("./db"));
+const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
 const routes_1 = __importDefault(require("./routes"));
 const middleware_1 = require("./middleware");
@@ -13,10 +14,11 @@ const app = (0, express_1.default)();
 db_1.default.connect();
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
+app.use((0, cors_1.default)());
 app.use(middleware_1.authenticate);
 app.use(routes_1.default);
-app.get("/", (_req, res) => {
-    res.sendStatus(404);
+app.get("*", (_req, res) => {
+    res.status(404).json({ message: "Page not found" });
 });
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
--- src/index.ts
+++ src/index.ts
@@ -12,12 +12,12 @@ db.connect();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(cors());
+app.use(cors());
 app.use(authenticate);
 app.use(routes);
 
-app.get("/", (_req, res) => {
-  res.sendStatus(404);
+app.get("*", (_req, res) => {
+  res.status(404).json({ message: "Page not found" });
 });
 
 app.listen(PORT, () => {
