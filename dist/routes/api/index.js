8d29dfaf495477e8d6e1e6eee81e1b1170ebccae
Robert Adams <adamsiii.robert@gmail.com>
2022-04-17 19:21:31 -0700

    removed build folder

--- dist/routes/api/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const api_1 = require("../../controllers/api");
-const router = (0, express_1.Router)();
-router.post("/translate", api_1.translate);
-router.delete("/translations/:id", api_1.deleteTranslation);
-exports.default = router;
