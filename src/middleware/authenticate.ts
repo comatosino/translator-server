1a3d0dc524d090bc7f2dfb56b8d70bc36274a530
Robert Adams <adamsiii.robert@gmail.com>
2022-04-17 19:17:31 -0700

    type cast decoded payload to appease compiler

--- src/middleware/authenticate.ts
+++ src/middleware/authenticate.ts
@@ -1,24 +1,19 @@
-import jwt from "jsonwebtoken";
-import { RequestHandler } from "express";
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { RequestHandler } from 'express';
 
 const SECRET_KEY = process.env.SECRET_KEY!;
 
 const authenticate: RequestHandler = (req, res, next) => {
   const bearer = req.headers.authorization;
-  const token = bearer?.split(" ")[1];
-
+  const token = bearer?.split(' ')[1];
   if (token) {
-    jwt.verify(token, SECRET_KEY, (err, payload: any) => {
-      if (err) {
-        res.status(403).json({ error: err.message });
-      } else {
-        req.userID = payload.userID; // TODO: add typing so payload accepts userID property
-        next();
-      }
+    jwt.verify(token, SECRET_KEY, (err, decoded) => {
+      err
+        ? res.status(403).json({ error: err.message })
+        : (req.userID = (decoded as JwtPayload).userID);
     });
-  } else {
-    next();
   }
+  next();
 };
 
 export default authenticate;
